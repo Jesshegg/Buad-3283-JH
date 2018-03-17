55a9b009cbf463cb36301fcf7b4ef794398e7f6f
Jesshegg <35411937+Jesshegg@users.noreply.github.com>
2018-03-17 13:38:38 -0500

    edit

--- Assignments/February_15/index.php
+++ Assignments/February_15/index.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
   <html>
     <head>
-      
+      <title>Masthead and Navigation</title>
+      <link rel="stylesheet" href="index.processed.css">
     </head>
+    <body>
+      <div class="Masthead"></div>
+      <div class="Navigation">
+        <ul>
+          <li class="navigation-item">Home</li>
+          <li class="navigation-item">About</li>
+          <li class="navigation-item">Contact</li>
+        </ul>
+      </div>
+      <div class="Content"></div>
+      <div class="Footer"></div>
+    </body>
   </html>
